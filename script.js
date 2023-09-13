84a93302b4cdc7770259b74295a3396aed9f8c13
Jose Luis <wildchamo@gmail.com>
2023-09-12 23:41:41 -0500

    actualización del sitio

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const url =
-  "https://youtube-v31.p.rapidapi.com/search?channelId=UCBVjMGOIkavEAhyqpxJ73Dw&part=snippet%2Cid&order=date&maxResults=50";
+  "https://youtube-v31.p.rapidapi.com/search?channelId=UCa-Q0JeeX_38r7qkdSpElWw&part=snippet%2Cid&order=date&maxResults=50";
 const options = {
   method: "GET",
   headers: {
@@ -7,14 +7,44 @@ const options = {
     "X-RapidAPI-Host": "youtube-v31.p.rapidapi.com",
   },
 };
+const content = null || document.getElementById("content");
 
-async function getData() {
-  try {
-    const response = await fetch(url, options);
-    const result = await response.text();
-    console.log(result);
-  } catch (error) {
-    console.error(error);
-  }
+async function getData(urlapi) {
+  const response = await fetch(urlapi, options);
+  const data = await response.json();
+  return data;
 }
- getData()
+
+(async () => {
+  try {
+    const videos = await getData(url);
+    console.log(videos);
+
+    let template = `
+     ${videos.items
+       .map(
+         (video) =>
+           `
+     <div class="group relative">
+     <div
+       class="w-full bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:aspect-none">
+       <img src="${video.snippet.thumbnails.high.url}" alt="${video.snippet.description}" class="w-full">
+     </div>
+     <div class="mt-4 flex justify-between">
+       <h3 class="text-sm text-gray-700">
+         <span aria-hidden="true" class="absolute inset-0"></span>
+         ${video.snippet.title}
+       </h3>
+     </div>
+   </div>
+     `
+       )
+       .slice(0, 10)
+       .join("")}
+`;
+    console.log(content);
+    content.innerHTML = template;
+  } catch (err) {
+    console.log(err);
+  }
+})();
