c1a0da107b3fb27bda69cb786f087f5d9fe3fb84
Jose Luis <wildchamo@gmail.com>
2023-09-14 13:05:07 -0500

    link a a los videos de yutu

--- script.js
+++ script.js
@@ -26,16 +26,18 @@ async function getData(urlapi) {
          (video) =>
            `
      <div class="group relative">
+     <a href='https://www.youtube.com/watch?v=${video.id.videoId}' target='_blanck'> 
      <div
-       class="w-full bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:aspect-none">
-       <img src="${video.snippet.thumbnails.high.url}" alt="${video.snippet.description}" class="w-full">
+     class="w-full bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:aspect-none">
+     <img src="${video.snippet.thumbnails.high.url}" alt="${video.snippet.description}" class="w-full">
      </div>
      <div class="mt-4 flex justify-between">
-       <h3 class="text-sm text-gray-300">
-         <span aria-hidden="true" class="absolute inset-0"></span>
-         ${video.snippet.title}
-       </h3>
+     <h3 class="text-sm text-gray-300">
+     <span aria-hidden="true" class="absolute inset-0"></span>
+     ${video.snippet.title}
+     </h3>
      </div>
+     </a>
    </div>
      `
        )
